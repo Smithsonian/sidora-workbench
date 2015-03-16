8af0a6bd12c3ad38eb91f088c8a608626d785793
Robert Anderson <randerson@quotient-inc.com>
2015-03-16 19:24:43 -0400

    Initial Delete XSL work

--- workbench/js/workbench_sidora_queue.js
+++ workbench/js/workbench_sidora_queue.js
@@ -118,7 +118,7 @@ SidoraQueue.prototype.NotificationWindow.UpdateTime = Date.now();
 SidoraQueue.prototype.NotificationWindow.Show = function(message, isError){
   var nw = this;
   if (jQuery('#queueMessage').length == 0){
-    var notificationWindowHtml = '<div id="queueMessage" style="display:none; position: fixed; bottom: 26px; right: 30px; min-height: 50px; width: 400px;"><div id="" role="tooltip" class="queue-message-block ui-tooltip ui-widget ui-corner-all ui-widget-content" style=" display: block;width: 100%;height: 100%;max-width: 1000px;"><div class="notification-window-message"></div><div class="queue-mb-close" style="background: url(http://***REMOVED***.my***REMOVED***.net/sites/all/modules/islandora_xml_forms-7.x/elements/css/images/ui-icons_222222_256x240.png) no-repeat -30px -191px;width: 20px;height: 20px;z-index: 100;position: absolute;right: 0;top: 0;"></div></div></div>';
+    var notificationWindowHtml = '<div id="queueMessage" style="display:none; position: fixed; bottom: 26px; right: 30px; min-height: 50px; width: 400px;"><div id="" role="tooltip" class="queue-message-block ui-tooltip ui-widget ui-corner-all ui-widget-content" style=" display: block;width: 100%;height: 100%;max-width: 1000px;"><div class="notification-window-message"></div><div class="queue-mb-close" style="background: url(/sites/all/modules/islandora_xml_forms-7.x/elements/css/images/ui-icons_222222_256x240.png) no-repeat -30px -191px;width: 20px;height: 20px;z-index: 100;position: absolute;right: 0;top: 0;"></div></div></div>';
     jQuery("body").append(notificationWindowHtml);
     jQuery("#queueMessage").mouseenter(function(){
       nw.MouseIsInside = true;
