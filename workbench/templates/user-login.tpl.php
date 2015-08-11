c93d4872892d39b1043359d978df78604a8c3bb2
Rashmi Amlani <ramlani@quotient-inc.com>
2015-08-11 19:32:42 -0400

    SID-398: Added an option to Sidora Configuration for the email address that new account requests should go to. Also added a mailto link to Contact us

--- workbench/templates/user-login.tpl.php
+++ workbench/templates/user-login.tpl.php
@@ -56,5 +56,6 @@ input.form-text {
   and my Email is:-Enter Email- and I would like to request a new SIdora Account"> Submit an Account Request</a><p>
 	-->
 	<p>Not a member? <a href="<?php echo base_path(); ?>sidora/request_account"> Submit an Account Request</a><p>
-<p>Need help? <a href="javascript:void(0);">Contact Us</a> <span style="color: #999;">(link to contact form)</span></p>
+<p>Need help? <?php $default_to = variable_get('system_mail', '***REMOVED***');
+echo '<a href="mailto:'.variable_get('account_request_email', $default_to).'?Subject=SIdora Account Help&body=My name is:-Enter Name- and my Email is:-Enter Email- and I am requesting help with my SIdora Account.  -Enter more information that indicates the kind of assistance you would like-)">Contact Us</a>';?></p>
 <!-- <p><a href="user/password">Request a New Password</a></p> -->
