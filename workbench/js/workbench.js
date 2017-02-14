c2052d2bf4bbbf47bc7a45446e992b6c49790bd8
ramlani <ramlani@quotient-inc.com>
2017-02-14 11:09:58 -0500

    SID-797:Changes to the OBJ upload dialog

--- workbench/js/workbench.js
+++ workbench/js/workbench.js
@@ -74,6 +74,7 @@ sidora.concept.LoadContentHelp.Resources.TableLoad = function(conceptOfInterest)
      })
   });
   var table = jQuery('#res_table').DataTable();
+	jQuery('#res_table').DataTable().clearPipeline().draw();
   jQuery('#res_table tbody').on( 'click', 'tr', function (e) {
     //If the mousedown is on something that is in the middle of a move process, ignore the mousedown
     if (jQuery(this).hasClass("is-being-moved")){
@@ -108,10 +109,9 @@ sidora.concept.LoadContentHelp.Resources.TableLoad = function(conceptOfInterest)
       jQuery("#manage-resource").addClass("ui-state-disabled");
 			jQuery("#edit-resource-datastream-menu").addClass("ui-state-disabled");
     }else{
-      sidora.resources.permission(sidora.resources.individualPanel.resourceOfInterest.pid);
       jQuery("#edit-resource-metadata-menu").removeClass("ui-state-disabled");
       jQuery("#manage-resource").removeClass("ui-state-disabled");
-			jQuery("#edit-resource-datastream-menu").addClass("ui-state-disabled");
+			jQuery("#edit-resource-datastream-menu").removeClass("ui-state-disabled");
     }
   }); //End onclick
     table.on( 'length', function ( e, settings, len ) {
@@ -1278,6 +1278,31 @@ sidora.resources.updateThumbnails = function(){
     }
   })
 }
+sidora.resources.refreshSelectedResourceThumbnail = function(){
+ console.log("in refreshSelectedResourceThumbnail");
+ var pids = sidora.resources.getHighlighted();
+ toRefreshPid = pids[0];
+  var me = jQuery(jq(toRefreshPid));
+  if (typeof(me.children("td").children("div").children("img").attr("src")) == 'string'){
+      var imgChild = me.children("td").children("div").children("img");
+      var pidThumbnail = Drupal.settings.basePath+"sidora/info/"+toRefreshPid+"/meta/TN/browser";
+      jQuery.ajax(pidThumbnail,{
+        complete:function(res){
+				  console.log("in refreshSelectedResourceThumbnail ajax "+res.getResponseHeader("content-type"));
+					//var currType = res.getResponseHeader("content-type");
+          //if (currType.indexOf("image") != -1){
+            setTimeout(function(){
+             sidora.resources.individualPanel.resourceOfInterest.pid = null;
+						 imgChild.attr("src",pidThumbnail).attr("height","").attr("width","").addClass("resource-list-tn");
+             console.log(imgChild.attr("src"));
+						 sidora.resources.individualPanel.resourceOfInterest.pid = toRefreshPid;
+					  },2000);
+					//}
+        }
+      });
+  }
+}
+
 /*
  * Get the pids of the highlighed resources
  */
@@ -2369,9 +2394,8 @@ sidora.resources.individualPanel.Create = function() {
   jQuery("#edit-resource-datastream-menu").click(function(){
     var resourcePid = sidora.resources.individualPanel.resourceOfInterest.pid;
     var resourceName = sidora.resources.individualPanel.resourceOfInterest.name;
-    sidora.manage.Open(resourcePid, resourceName, "Add OBJ", "Update OBJ datastream");
     jQuery('#addDatastreamDialog').remove();
-    jQuery("body").append("<div id='addDatastreamDialog' style='display:none;' title='Add OBJ'><iframe height='1000%' width='100%' style='height:100%;width:100%' src='"+Drupal.settings.basePath+"sidora/manage/"+resourcePid+"/upload/OBJ' frameborder='0' marginwidth='0' marginheight='0' allowfullscreen></iframe></div>");
+    jQuery("body").append("<div id='addDatastreamDialog' style='display:none;' title='Update Content'><iframe height='1000%' width='100%' style='height:100%;width:100%' src='"+Drupal.settings.basePath+"sidora/manage/"+resourcePid+"/upload_content' frameborder='0' marginwidth='0' marginheight='0' allowfullscreen></iframe></div>");
     jQuery("#addDatastreamDialog").dialog({
      resizable: true,
      height:600,
@@ -2819,13 +2843,4 @@ sidora.reloadPage = function(){
 }
 sidora.util.refreshPidInTree = function(){
 sidora.util.RefreshTree(null,sidora.concept.GetPid());
-}
-sidora.resources.permission = function(resourceOfInterest){
-  jQuery.ajax({
-    dataType: "json",
-    url: Drupal.settings.basePath+'sidora/info/'+resourceOfInterest+'/updateDS',
-    success: function(objPermissions){
-      jQuery("#edit-resource-datastream-menu").toggleClass('ui-state-disabled',objPermissions.ui_state_disable);
-    }
-  });
 }
